78161f7e9109eae1d884cb0e4bd0627102528eaa
smtran8 <davidtran.forwork@gmail.com>
2025-04-28 12:06:06 -0400

    Update testing.py

--- data_handling/testing.py
+++ data_handling/testing.py
@@ -1,2 +1,25 @@
+import numpy as np
+n_rows = 10
 
+duration_min = np.random.randint(10, 180, size=n_rows)
+priority = np.random.randint(1, 11, size=n_rows)
+energy_required = np.random.randint(1, 11, size=n_rows)
+available_minutes = np.random.randint(30, 600, size=n_rows)
+success = np.random.choice([0, 1], size=n_rows, p=[0.5, 0.5])
 
+data_testing = []
+for i in range(n_rows):
+    row = [
+        f"Task {i+1}",
+        int(duration_min[i]),
+        int(priority[i]),
+        int(energy_required[i]),
+        int(available_minutes[i]),
+        int(success[i]),
+    ]
+    data_testing.append(row)
+
+columns = ["Task_Name", "Duration", "Priority", "Energy Requirements", "Available Minutes", "Success"]
+print("\t".join(columns))
+for row in data_testing:
+    print("\t".join(str(x) for x in row))
